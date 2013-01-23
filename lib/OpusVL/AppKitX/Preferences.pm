66d850626430f01254b13f0ef3637f46a3af32b3
Colin Newell <colin.newell@gmail.com>
2013-01-23 12:48:15 +0000

    Initial checkin

---
+++ lib/OpusVL/AppKitX/Preferences.pm
@@ -0,0 +1,45 @@
+package OpusVL::AppKitX::Preferences;
+use Moose::Role;
+use CatalystX::InjectComponent;
+use File::ShareDir qw/module_dir/;
+use namespace::autoclean;
+
+with 'OpusVL::AppKit::RolesFor::Plugin';
+
+our $VERSION = '0.01';
+
+after 'setup_components' => sub {
+    my $class = shift;
+   
+    $class->add_paths(__PACKAGE__);
+    
+    # .. inject your components here ..
+    CatalystX::InjectComponent->inject(
+        into      => $class,
+        component => 'OpusVL::AppKitX::Preferences::Controller::Preferences',
+        as        => 'Controller::Modules::Preferences'
+    );
+};
+
+1;
+
+=head1 NAME
+
+OpusVL::AppKitX::Preferences - 
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head1 BUGS
+
+=head1 AUTHOR
+
+=head1 COPYRIGHT and LICENSE
+
+Copyright (C) 2013 OpusVL
+
+This software is licensed according to the "IP Assignment Schedule" provided with the development project.
+
+=cut
+
---
+++ lib/OpusVL/AppKitX/Preferences/Controller/Preferences.pm
@@ -0,0 +1,46 @@
+package OpusVL::AppKitX::Preferences::Controller::Preferences;
+
+use Moose;
+use namespace::autoclean;
+BEGIN { extends 'Catalyst::Controller'; };
+with 'OpusVL::AppKit::RolesFor::Controller::GUI';
+
+__PACKAGE__->config
+(
+    appkit_name                 => 'Preferences',
+    # appkit_icon                 => 'static/images/flagA.jpg',
+    appkit_myclass              => 'OpusVL::AppKitX::Preferences::Preferences',
+    # appkit_method_group         => 'Extension A',
+    # appkit_method_group_order   => 2,
+    # appkit_shared_module        => 'ExtensionA',
+);
+
+# sub home
+#     :Path
+#     :Args(0)
+#     :NavigationHome
+#     :AppKitFeature('Extension A')
+# {
+#     my ($self, $c) = @_;
+# }
+
+=head1 NAME
+
+OpusVL::AppKitX::Preferences::Controller:Preferences - 
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head1 BUGS
+
+=head1 AUTHOR
+
+=head1 COPYRIGHT and LICENSE
+
+Copyright (C) 2013 OpusVL
+
+This software is licensed according to the "IP Assignment Schedule" provided with the development project.
+
+=cut
+
